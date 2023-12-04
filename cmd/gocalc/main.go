7ec1a261270300ff48e21ba29e4bf4ae2e880c64
Philip Cunningham <pcunningham@gitlab.com>
2023-12-04 17:49:13 +0000

    Add spew

--- cmd/gocalc/main.go
+++ cmd/gocalc/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/davecgh/go-spew/spew"
 	"github.com/mcfilib/gocalc/lexer"
 )
 
 func main() {
 	result := lexer.Lex([]rune("(+ 1 2 3 4)"))
 
-	fmt.Println(result)
+	spew.Dump(result)
 }
