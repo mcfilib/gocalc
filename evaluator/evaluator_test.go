08286dfceca992a7a6150e616974013a826ba766
Philip Cunningham <pcunningham@gitlab.com>
2023-12-05 13:24:58 +0000

    Add nested example to Eval test

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -10,12 +10,12 @@ import (
 )
 
 func TestEval(t *testing.T) {
-	tokens := lexer.Lex([]rune("(+ 1 2 3 4)"))
+	tokens := lexer.Lex([]rune("(+ 1 2 3 (+ 4 5 6 (+ 7 8 9)))"))
 	require.NotNil(t, tokens)
 
 	_, ast := parser.Parse(0, tokens)
 	require.NotNil(t, ast)
 
 	got := evaluator.Eval(ast)
-	require.Equal(t, 10, got)
+	require.Equal(t, 45, got)
 }
